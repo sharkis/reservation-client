6efd85135782e8cd198f55e211d51ce454d50910
Albert Sharkis <sharkis@sharkis.org>
2023-03-27 13:07:22 -0600

    table edit dialog

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 
 import {
-  Button, Snackbar, Alert, Dialog,
+  Button, Snackbar, Alert, Dialog, TextField,
 } from '@mui/material';
 import {
-  Layer, Stage, Rect, Shape,
+  Layer, Stage, Rect, Shape, Text,
 } from 'react-konva';
 import axios from 'axios';
 import { v4 } from 'uuid';
@@ -15,17 +15,32 @@ import Header from '../components';
 
 const API_URL = `${process.env.REACT_APP_API_URL}/tables`;
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'setTable':
+      return action.payload;
+    case 'setName':
+      return {
+        ...state,
+        name: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
 function Layout() {
   const [tables, setTables] = useState([]);
+  const [table, dispatch] = useReducer(reducer, {});
   const [showSnack, setShowSnack] = useState(false);
   const [showTableDialog, setShowTableDialog] = useState(false);
 
   const handleDragStart = (e) => {
     const id = e.target.id();
     setTables(
-      tables.map((table) => ({
-        ...table,
-        isDragging: table.id === id,
+      tables.map((t) => ({
+        ...t,
+        isDragging: t.id === id,
       })),
     );
   };
@@ -49,7 +64,7 @@ function Layout() {
     axios
       .post(API_URL, tables)
       .then((res) => {
-        if (res.statusCode === 200) {
+        if (res.status === 200) {
           setShowSnack(true);
         }
       })
@@ -60,6 +75,20 @@ function Layout() {
 
   const addTable = () => {
     setTables((t) => [...t, { x: 0, y: 0, id: v4() }]);
+  };
+
+  const updateTable = () => {
+    const newTables = [...tables];
+    const { id } = table;
+    const newidx = tables.findIndex((t) => t.id === id);
+    newTables[newidx] = {
+      ...tables[newidx],
+      name: table.name,
+      capacity: table.capacity,
+    };
+    setTables(newTables);
+    setShowTableDialog(false);
+    dispatch({ type: 'setTable', payload: {} });
   };
 
   useEffect(() => {
@@ -76,8 +105,14 @@ function Layout() {
 
   return (
     <Authenticator>
-      <Dialog open={showTableDialog} onClose={() => setShowTableDialog(false)}>
-        table stuff
+      <Dialog open={showTableDialog} onClose={() => { setShowTableDialog(false); dispatch({ type: 'setTable', payload: {} }); }}>
+        <Button>ror</Button>
+        <Button>rol</Button>
+        <TextField placeholder="capacity" value={table.capacity} onChange={(e) => { dispatch({ type: 'setCapacity', payload: e.target.value }); }} />
+        <TextField placeholder="name" value={table.name} onChange={(e) => dispatch({ type: 'setName', payload: e.target.value })} />
+        <Button onClick={updateTable}>
+          Save Table
+        </Button>
       </Dialog>
       <Header />
       <Button onClick={addTable}>Add Table</Button>
@@ -98,20 +133,23 @@ function Layout() {
             stroke="#000"
             fill="#ccc"
           />
-          {tables.map((table) => (
-            <Rect
-              key={table.id}
-              id={table.id}
-              x={table.x}
-              y={table.y}
-              width={100}
-              height={50}
-              fill="#000"
-              draggable
-              onDragStart={handleDragStart}
-              onDragEnd={handleDragEnd}
-              onClick={() => setShowTableDialog(true)}
-            />
+          {tables.map((t) => (
+            <>
+              <Rect
+                key={t.id}
+                id={t.id}
+                x={t.x}
+                y={t.y}
+                width={100}
+                height={50}
+                fill="#000"
+                draggable
+                onDragStart={handleDragStart}
+                onDragEnd={handleDragEnd}
+                onClick={() => { setShowTableDialog(true); dispatch({ type: 'setTable', payload: t }); }}
+              />
+              <Text text={t.name} x={t.x} y={t.y} fill="#fff" />
+            </>
           ))}
         </Layer>
       </Stage>
